310681feff285125c433f3c9e7486a3dc03376bd
ruperto arrieta jr <totoarrieta@yahoo.com>
2022-03-21 15:58:54 +0800

    added pri key

--- tutorials/serializers.py
+++ tutorials/serializers.py
@@ -1,21 +1,25 @@
-from rest_framework import serializers
+#from rest_framework import serializers
 from tutorials.models import Tutorial
 from tutorials.models import Notes27Jan
- 
+''' test
+
 class TutorialSerializer(serializers.ModelSerializer):
- 
     class Meta:
         model = Tutorial
         fields = ('id',
                   'title',
                   'description',
                   'published')
-
 class Notes27JanSerializer(serializers.ModelSerializer):
- 
     class Meta:
         model = Notes27Jan
         fields = ('id',
                   'title',
                   'desc',
                   'date')
+
+'''
+ 
+ 
+
+ 
